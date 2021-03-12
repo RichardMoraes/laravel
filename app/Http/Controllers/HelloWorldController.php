adb0c9830741c1d32a589a78686fea3cd6623ad8
Richard Lima <richardlima6997@gmail.com>
2021-03-12 11:44:36 -0300

    First Commit

---
+++ app/Http/Controllers/HelloWorldController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloWorldController extends Controller
+{
+    public function helloWorld() {
+         return view('hello_world');
+    }
+    public function hello($name = 'Fulano') {
+         return 'Heloo, ' . $name;
+    }
+}
